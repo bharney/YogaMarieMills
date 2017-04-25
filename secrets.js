8d0570cac2f6139bb5def7cdc638185f3217d3c4
bharney <bharney0@gmail.com>
2017-04-25 08:08:20 -0700

    removed gitignore files

--- secrets.js
+++
@@ -1,5 +0,0 @@
-export const secret = '734ac163eb05d6501c102cc25d6afb348c334b6f591f8875428fe49352f00af5';
-
-export const dbconfig = "mssql://Application:!Testing123@BPHSERVER/YogaMarieMills";
-
-export default secret;
